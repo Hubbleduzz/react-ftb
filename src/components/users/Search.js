124eb9f14566ab106484bb904cec44c6710079ed
Hubbleduzz <mordenjosh@gmail.com>
2020-12-31 20:35:20 +0000

    27: User class to function with hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,4 +1,58 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
+  const [text, setText] = useState("");
+
+  const onChange = (e) => setText(e.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      setAlert("Please enter something", "light");
+    } else {
+      searchUsers(text);
+      setText("");
+    }
+  };
+  return (
+    <div>
+      <form className="form" onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default Search;
+
+/**
+ * Class based component, pre-refactorisation for functional component with utilising hooks and context api
+ * 
+ * import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export class Search extends Component {
@@ -54,3 +108,5 @@ export class Search extends Component {
 }
 
 export default Search;
+
+ */
