3d689c0297f4b3ecd502e51991bf7236175130a1
Hubbleduzz <mordenjosh@gmail.com>
2020-12-31 00:16:10 +0000

    Starting Section 5: Refactoring - Hooks & Context - should be bug free

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -63,12 +64,12 @@ class App extends Component {
   getUserRepos = async (username) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?client_id=
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
       ${process.env.REACT_APP_GITHUB_CLIENT_ID}
       &client_secret=
       ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}}`
     );
-    this.setState({ user: res.data, loading: false });
+    this.setState({ repos: res.data, loading: false });
   };
 
   clearUsers = () => {
@@ -81,7 +82,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading, user } = this.state;
+    const { users, loading, user, repos } = this.state;
     const { clearUsers, searchUsers } = this;
 
     return (
@@ -114,7 +115,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
