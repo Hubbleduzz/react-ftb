654143bab73003bb03bfbaa54c1fb3136edea43b
Hubbleduzz <mordenjosh@gmail.com>
2021-01-03 18:33:16 +0000

    29: Implementing Context - back on track, one small bug after refactoring APP, namely, no list of misc users loading on home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,202 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import "./CRApp.css";
+import axios from "axios";
+import User from "./components/users/User";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
+
+  // Get GitHub Users
+
+  const searchUsers = async (text) => {
+    setLoading(true);
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}}`
+    );
+
+    setUsers(res.data.items);
+    setLoading(false);
+  };
+
+  // Get single Github user
+
+  const getUser = async (username) => {
+    setLoading(true);
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}}`
+    );
+
+    setUser(res.data);
+    setLoading(false);
+  };
+
+  // Get users repos
+
+  const getUserRepos = async (username) => {
+    setLoading(true);
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}}`
+    );
+    setRepos(res.data);
+    setLoading(false);
+  };
+
+  const clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
+  };
+
+  const showAlert = (msg, type) => {
+    setAlert({ msg, type });
+    setTimeout(() => setAlert(null), 5000);
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Alert alert={alert} />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <Fragment>
+                  <Search
+                    searchUsers={searchUsers}
+                    clearUsers={clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={showAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
+            />
+            <Route exact path="/about" component={About} />
+            <Route
+              exact
+              path="/user/:login"
+              render={(props) => (
+                <User
+                  {...props}
+                  getUser={getUser}
+                  getUserRepos={getUserRepos}
+                  user={user}
+                  repos={repos}
+                  loading={loading}
+                />
+              )}
+            />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+// CONCERNING TWO APPROACHES TO THE IMPLEMENTATION OF THE 'ROUTE' COMPONENT
+
+// The first approach is neccessary when iteration between the two components
+// is required i.e., when props need to be passed in (as in the first case)
+
+export default App;
+
+/**Notes
+ *
+ * When employing a conditional statement which has no else clause, you can replace
+ * the ternary expression or the standard conditional statement with a double ampersand
+ * instead.
+ * (CONSULT ELOQUENT JAVASCRIPT FIRST CHAPTER FOR AN EXPLANATION OF WHY THIS IS POSSIBLE)
+ */
+
+// Functional/ non-class based component
+// import React from "react";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello from Visual Studio Code!</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// class App extends Component {
+//   nameGenerator = (name) => name.toUpperCase();
+
+//   render() {
+//     const nameOne = "John";
+//     const loadingBoolean = false;
+//     const showName = true;
+
+//     return (
+//       <div className="App">
+//         <h1> Greetings!</h1>
+//         {loadingBoolean ? (
+//           <h2>Loading...(We're trying to find out your name!)</h2>
+//         ) : (
+//           <h1>
+//             {showName && this.nameGenerator(nameOne)} from Visual Studio Code!
+//           </h1>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+// async componentDidMount() {
+//   this.setState({ loading: true });
+
+//   const res = await axios.get("https://api.github.com/users");
+
+//   console.log(res.data);
+
+//   let data = res.data;
+
+//   let userNames = [];
+
+//   for (const key in data) {
+//     let value = data[key];
+//     //console.log(value.login);
+//     userNames.push(value.login);
+//   }
+
+//   this.setState({ userNames: userNames, loading: false });
+
+//   console.log(this.state);
+// }
+
+/**
+ * Pre Refactor
+ *
+ *import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
@@ -136,72 +334,4 @@ class App extends Component {
 // is required i.e., when props need to be passed in (as in the first case)
 
 export default App;
-
-/**Notes
- *
- * When employing a conditional statement which has no else clause, you can replace
- * the ternary expression or the standard conditional statement with a double ampersand
- * instead.
- * (CONSULT ELOQUENT JAVASCRIPT FIRST CHAPTER FOR AN EXPLANATION OF WHY THIS IS POSSIBLE)
  */
-
-// Functional/ non-class based component
-// import React from "react";
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello from Visual Studio Code!</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// class App extends Component {
-//   nameGenerator = (name) => name.toUpperCase();
-
-//   render() {
-//     const nameOne = "John";
-//     const loadingBoolean = false;
-//     const showName = true;
-
-//     return (
-//       <div className="App">
-//         <h1> Greetings!</h1>
-//         {loadingBoolean ? (
-//           <h2>Loading...(We're trying to find out your name!)</h2>
-//         ) : (
-//           <h1>
-//             {showName && this.nameGenerator(nameOne)} from Visual Studio Code!
-//           </h1>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-// async componentDidMount() {
-//   this.setState({ loading: true });
-
-//   const res = await axios.get("https://api.github.com/users");
-
-//   console.log(res.data);
-
-//   let data = res.data;
-
-//   let userNames = [];
-
-//   for (const key in data) {
-//     let value = data[key];
-//     //console.log(value.login);
-//     userNames.push(value.login);
-//   }
-
-//   this.setState({ userNames: userNames, loading: false });
-
-//   console.log(this.state);
-// }
